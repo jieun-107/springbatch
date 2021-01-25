d00600f260b0642639dab1943988e56d0ae1d35f
jelee <jieun@ijieuns-MacBook-Pro.local>
2021-01-26 01:11:39 +0900

    Initial commit

---
+++ src/main/java/com/jelee/spring/springbatch/KoreaStats.java
@@ -0,0 +1,11 @@
+package com.jelee.spring.springbatch;
+
+public class KoreaStats {
+    /* 시도명 */
+    private String contry;
+    /* 전일대비 확진자 증감 */
+    private int diiffFromPrevDay;
+    /* 확진환자수 */
+    private int total;
+    
+}
