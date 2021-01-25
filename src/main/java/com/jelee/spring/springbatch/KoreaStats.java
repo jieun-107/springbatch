c5695f8004c13753c5d481d4b507b5ceaa3e133b
jieun <lje7128@gmail.com>
2021-01-26 01:17:32 +0900

    객체 생성

--- src/main/java/com/jelee/spring/springbatch/KoreaStats.java
+++ src/main/java/com/jelee/spring/springbatch/KoreaStats.java
@@ -1,5 +1,12 @@
 package com.jelee.spring.springbatch;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Getter
 public class KoreaStats {
     /* 시도명 */
     private String contry;
@@ -7,5 +14,10 @@ public class KoreaStats {
     private int diiffFromPrevDay;
     /* 확진환자수 */
     private int total;
-    
+    /* 사망자 수 */
+    private int death;
+    /* 발병률 */
+    private double incidence;
+    /* 일일 검사환자  */
+    private int inspection;
 }
