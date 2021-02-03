75ca88fe8d5d74c2ba71c79fdd67ed800b3090bd
jieun <lje7128@gmail.com>
2021-02-04 01:58:51 +0900

    서비스 리턴값 수정

--- src/main/java/com/jelee/spring/springbatch/CoronaVirusDataService.java
+++ src/main/java/com/jelee/spring/springbatch/CoronaVirusDataService.java
@@ -8,32 +8,35 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
     private static String KOREA_COVID_DATAS_URL = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13";
 
     @PostConstruct
-    public void getKoreaCovidDatas() throws IOException {
+    public List<KoreaStats> getKoreaCovidDatas() throws IOException {
         Document doc = Jsoup.connect(KOREA_COVID_DATAS_URL).get();
         Elements contents = doc.select("table tbody tr");
 
+        List<KoreaStats> statsList = new ArrayList<KoreaStats>();
         for(Element content : contents){
             Elements tdContents = content.select("td");
 
             KoreaStats stats = KoreaStats.builder()
-                    .contry(content.select("th").text())
-                    .diiffFromPrevDay(Integer.parseInt(tdContents.get(0).text()))
+                    .country(content.select("th").text())
+                    .diffFromPrevDay(Integer.parseInt(tdContents.get(0).text()))
                     .total(Integer.parseInt(tdContents.get(1).text().replace(",", "")))
                     .death(Integer.parseInt(tdContents.get(2).text().replace(",", "")))
                     .incidence(Integer.parseInt(tdContents.get(3).text().replace(",", "")))
                     .inspection(Integer.parseInt(tdContents.get(4).text().replace(",", "")))
                     .build();
 
-            System.out.println(stats.toString());
-
+            statsList.add(stats);
         }
 
+        return statsList;
     }
 
 
